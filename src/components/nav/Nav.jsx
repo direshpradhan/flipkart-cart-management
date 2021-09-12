1869e856f6c16bea24161edff9df007016d48f9b
Diresh Kumar <41542536+diresh18@users.noreply.github.com>
2021-09-12 13:25:55 +0530

    added pricing details

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -4,7 +4,7 @@ import styles from "./Nav.module.css";
 export const Nav = ({ setRoute }) => {
   return (
     <div className={`${styles.navigation}`}>
-      <h2>Flipkart Clone</h2>
+      <h2 onClick={() => setRoute("home")}>Flipkart Clone</h2>
       <div className={`${styles.link}`}>
         <p onClick={() => setRoute("home")}>Home</p>
         <p onClick={() => setRoute("cart")}>Cart</p>
--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -7,18 +7,54 @@ import { SaveLaterCard } from "./components/saveLaterCard/SaveLaterCard";
 export const Cart = () => {
   const { cart, saveLater } = useData();
 
+  const totalPrice = (arr) => {
+    return arr.reduce(
+      (total, { quantity, price }) => total + quantity * price,
+      0
+    );
+  };
+
+  const totalDiscountedPrice = (arr) => {
+    return arr.reduce(
+      (total, { quantity, discountedPrice }) =>
+        total + quantity * discountedPrice,
+      0
+    );
+  };
+
   return (
     <>
-      <div>
+      <div className={`${styles.cart}`}>
         <div className={`${styles.card_container}`}>
           <h2>My Cart</h2>
-          {cart.map((cartItem) => {
-            return <CartCard cartItem={cartItem} />;
-          })}
+          {cart.length > 0 ? (
+            cart.map((cartItem) => {
+              return <CartCard cartItem={cartItem} />;
+            })
+          ) : (
+            <p>No items in cart</p>
+          )}
         </div>
-        <div> </div>
+        {cart.length > 0 && (
+          <div className={`${styles.pricing}`}>
+            <h2>Price:</h2>
+            <h4>Total Items: {cart.length}</h4>
+            <div className={`${styles.prices}`}>
+              <div>
+                <p>Price:</p>
+                <p>Discount:</p>
+                <p>Total Price:</p>
+              </div>
+              <div>
+                <p>{totalPrice(cart)}</p>
+                <p>{totalPrice(cart) - totalDiscountedPrice(cart)}</p>
+                <p>{totalDiscountedPrice(cart)}</p>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
-      <div>
+      <div className={`${styles.saveLater}`}>
         <div className={`${styles.card_container}`}>
           <h2>Save For Later</h2>
           {saveLater.map((saveItem) => {
